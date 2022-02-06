5bbbff6c4a9f5318bfbc77997a1de1a728d8fd5e
Mourad Sherif <mouradsherif25@outlook.com>
2022-02-07 00:31:14 +0200

    Added server file

--- server.js
+++ server.js
@@ -1,29 +1,7 @@
 var express = require('express');
-var app = express()
-var ejs = require('ejs');
+var app = express();
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
 
-app.use( express.static( "./views/components/public" ) );
+app.use(bodyParser.urlEncoded({ extended: true }))
 app.set('view engine', 'ejs')
-
-//Landing Page
-app.get('/', (req, res) => res.render('landing', {page: 'Landing'}))
-
-//Home Page
-app.get('/home', (req, res) => res.render('home', { page: 'Home' }))
-
-//Student Navbar Component
-app.get('/navbar', (req, res) => res.render('components/lesson-card/lesson-card.ejs', {page: 'Navbar'}))
-
-//Filter Component
-app.get('/filter', (req, res) => res.render('components/filter/filter.ejs', { page: "filter" }))
-
-
-
-
-//Server Port
-let port = 3000;
-
-//Server Listener
-app.listen(port, () => {
-    console.log(`Server started at port ${port}`)
-})
